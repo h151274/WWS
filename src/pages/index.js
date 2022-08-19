2a327a8745f73aaad2bf39327165d778f6978147
h151274 <aarsheimbadmin@odfjell.com>
2022-08-19 14:22:55 +0200

    fix directory name case error

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,36 @@
-import React, {useState} from 'react'
-import Sidebar from '../components/Sidebar'
-import Navbar from '../components/Navbar'
+import React, { useState } from 'react';
+import Footer from '../components/Footer';
 import FrontSection from '../components/FrontSection';
 import InfoSection from '../components/InfoSection';
-import { homeObjOne, homeObjTwo} from '../components/InfoSection/Data';
+import { homeObjOne } from '../components/InfoSection/Data';
+import Navbar from '../components/Navbar';
 import PictureSection from '../components/PictureSection';
-import { WeddingInfo } from '../components/Wedding';
 import { infoSectionObjOne, infoSectionObjTwo } from '../components/PictureSection/Data';
+import Sidebar from '../components/Sidebar';
 import TextSection from '../components/TextSection';
 import { textObjOne } from '../components/TextSection/Data';
-import Footer from '../components/Footer';
+import { WeddingInfo } from '../components/Wedding';
 
 
 const Home = () => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggle = () => { 
-        setIsOpen(!isOpen)
-    }
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
 
   return (
     <>
-    <Sidebar isOpen={isOpen} toggle={toggle}/>
-    <Navbar toggle={toggle}/>
-    <FrontSection/>
-    <WeddingInfo /> 
-    <PictureSection {...infoSectionObjOne}/>    
-    <TextSection {...textObjOne}/>
-    <PictureSection {...infoSectionObjTwo}/>    
-    <InfoSection {...homeObjOne}/>
-    <Footer/>
-    
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Navbar toggle={toggle} />
+      <FrontSection />
+      <WeddingInfo />
+      <PictureSection {...infoSectionObjOne} />
+      <TextSection {...textObjOne} />
+      <PictureSection {...infoSectionObjTwo} />
+      <InfoSection {...homeObjOne} />
+      <Footer />
+
     </>
   )
 }
