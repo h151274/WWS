dfbf53d41c6e992bb86eb9487ade91e414428ba0
Bonn Aarsheim <bonn.aarsheim@rainfall.no>
2024-03-11 13:25:45 +0100

    information update fix

--- src/components/Wedding/WeddingInfo.js
+++ src/components/Wedding/WeddingInfo.js
@@ -21,7 +21,7 @@ export const WeddingInfo = () => {
 
         <WeddingContainer id="Wedding">
             <WeddingH1>Please join us on 30th March, 2024 as we celebrate our wedding in beautiful Koh Samui, Thailand.</WeddingH1>
-            <WeddingTopLine>Dress code: Beach Casual</WeddingTopLine>
+            <WeddingTopLine>Dress code: Beach Casual/Beach Cocktail</WeddingTopLine>
 
             <NavBtnLink to='/rsvp'> RSVP by 30th November </NavBtnLink>
 
@@ -29,14 +29,14 @@ export const WeddingInfo = () => {
             <WeddingWrapper>
                     <WeddingCard>
                         <WeddingPicture src={bilde1} />
-                        <WeddingH2>Welcome Party</WeddingH2>
-                        <WeddingTopLine>18.00 - 23.00</WeddingTopLine>
-                        <WeddingP> Sun Lounge at Silavadee Pool Spa Resort, Koh Samui.</WeddingP>
+                        <WeddingH2>Ceremony</WeddingH2>
+                        <WeddingTopLine>15.30 - 16.30</WeddingTopLine>
+                        <WeddingP> Private Beach at Silavadee Pool Spa Resort, Koh Samui.</WeddingP>
                     </WeddingCard>
                     <WeddingCard>
                         <WeddingPicture src={bilde2} />
-                        <WeddingH2>Ceremony</WeddingH2>
-                        <WeddingTopLine>15.30 - 16.00</WeddingTopLine>
+                        <WeddingH2>Champagne Hour</WeddingH2>
+                        <WeddingTopLine>16.30 - 18.30</WeddingTopLine>
                         <WeddingP> Private Beach at Silavadee Pool Spa Resort, Koh Samui.</WeddingP>
                     </WeddingCard>
                     <WeddingCard>
